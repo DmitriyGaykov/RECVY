2cad328c942d4e09af32ed6c21073780bd2d9291
Dmitriy <dima.gaykov.04@gmail.com>
2023-11-14 22:32:56 +0300

    Start people page

---
+++ frontend/src/pages/main-page/modules/people-page/index.tsx
@@ -0,0 +1,7 @@
+export const SubscribersPage = () => {
+  return (
+    <div className="w-100 flex-1 d-flex p-3 justify-content-start align-items-start">
+      <input className="form-text"/>
+    </div>
+  )
+}
